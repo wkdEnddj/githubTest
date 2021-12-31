e557ddf962fc54c97a97c5803d7b0ca1426a270d
qkrrm <qkrrm@DESKTOP-TP1NGUB>
2021-12-31 14:46:54 +0900

    Change Code

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("GeunWoo Park");
+		System.out.println("¹Ú±Ù¿ì");
 	}
 
 }
